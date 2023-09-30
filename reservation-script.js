de743742847866d0e99171c8f830e66b92bff1c9
Alexedpx <alexandradepourtoux93@gmail.com>
2023-09-30 12:44:50 +0200

    ajout de la pop up

--- reservation-script.js
+++ reservation-script.js
@@ -1,20 +1,13 @@
 // JAVASCRIPT RESERVATION & FORMULAIRE ALEXANDRA //
 
-document.getElementById("contactForm").addEventListener("submit", function (event){
-event.preventDefault ();
 
 
+function popup() {
+    el = document.getElementById("popup");
+    el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
+  }
 
-const name = document.getElementById ("name").value;
-const email = document.getElementById ("email").value;
-const message = document.getElementById ("message").value;
-
-
-
-if (name === "" || email === "" || message === "") {
-alert('Formulaire invalide. Veuillez remplir tous les champs.');
-} else {
-alert('Merci pour votre message. Nous reviendrons vers vous rapidement !');
-}
-
-});
+ 
+    
+    
+    
