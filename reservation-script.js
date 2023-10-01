a5550d17bfff63a86c3e5489940a41317803fb0b
Alexedpx <alexandradepourtoux93@gmail.com>
2023-10-01 13:22:31 +0200

    Partie réservation & contact Alexandra terminée

--- reservation-script.js
+++ reservation-script.js
@@ -1,12 +1,11 @@
-// JAVASCRIPT RESERVATION & FORMULAIRE ALEXANDRA //
-
-
+// DEBUT JAVASCRIPT POP UP  ALEXANDRA //
 
 function popup() {
     el = document.getElementById("popup");
     el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
   }
 
+// FIN JAVASCRIPT POP UP  ALEXANDRA //
  
     
     
